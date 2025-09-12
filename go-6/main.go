6e24f6b1fa1bca0ef37280e91250ffc3d6b8098b
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-09-12 21:21:22 +0600

    go-6

---
+++ go-6/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var a = 10
+var b = 20
+
+func main() {
+	fmt.Println("Hello, init function!")
+	fmt.Println("a + b =", a+b)
+
+	func(x, y int) {
+		fmt.Println("Anonymous function called!", x, y)
+	}(a, b)
+}
+
+func init() {
+	fmt.Println("Init function called!", a, b)
+	a = 100
+	b = 200
+}
