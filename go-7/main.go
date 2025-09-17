b08bea045718d2e9640b9c5e1141d7186bfa3ed2
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-09-17 22:18:40 +0600

    closer

---
+++ go-7/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+const a = 10
+
+var p = 20
+
+func outer() func() {
+	mamory := 100
+	age := 18
+
+	fmt.Println("outer function called!", age)
+
+	show := func() {
+		fmt.Println("call function called!", mamory)
+		mamory = mamory + a + p
+		fmt.Println("memory:", mamory)
+	}
+	return show
+}
+
+func call() {
+	show1 := outer()
+	show1()
+	show1()
+	show1()
+
+	show2 := outer()
+	show2()
+	show2()
+
+}
+
+func main() {
+	call()
+}
