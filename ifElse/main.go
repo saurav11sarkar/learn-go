07219425a26549b312dd279dff478dfeffcfec8d
saurav11sarkar <sarkarsaurav11@outlook.com>
2025-10-10 14:38:02 +0600

    switch

--- ifElse/main.go
+++ ifElse/main.go
@@ -1,20 +1,57 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Enter your Roll: ")
+// 	var roll string
+// 	fmt.Scanln(&roll)
+// 	fmt.Println("Enter your Password: ")
+// 	var password string
+// 	fmt.Scanln(&password)
+
+// 	if roll == "admin" && password == "admin123" {
+// 		fmt.Println("Welcome Admin")
+// 	} else {
+// 		for range 3 {
+// 			fmt.Println("Invalid Credentials")
+// 		}
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Enter your Roll: ")
-	var roll string
+	var roll int
 	fmt.Scanln(&roll)
-	fmt.Println("Enter your Password: ")
-	var password string
-	fmt.Scanln(&password)
 
-	if roll == "admin" && password == "admin123" {
-		fmt.Println("Welcome Admin")
-	} else {
-		for range 3 {
-			fmt.Println("Invalid Credentials")
+	switch roll {
+	case 1:
+		fmt.Println("Enter your Password: ")
+		var password string
+		fmt.Scan(&password)
+		if password == "admin123" {
+			fmt.Println("Welcome Admin")
+		} else {
+			for range 3 {
+				fmt.Println("Invalid Credentials")
+			}
 		}
+	case 2:
+		fmt.Println("Enter your Password: ")
+		var password string
+		fmt.Scan(&password)
+		if password == "user123" {
+			fmt.Println("Welcome User")
+		} else {
+			for range 3 {
+				fmt.Println("Invalid Credentials")
+			}
+		}
+	default:
+		fmt.Println("Invalid Roll")
 	}
 }
